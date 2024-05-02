6ecbe98bbd7d5366724d7f5f5d9b4ec37c6f2823
cherry00y <anuthida.w64@rsu.ac.th>
2024-05-02 22:53:01 +0700

    .

--- index.js
+++ index.js
@@ -164,6 +164,6 @@ app.delete('/favourite', (req, res) => {
     )
 })
 
-app.listen(process.env.PORT || 3002, () => {
-    console.log('CORS-enabled web server listening on port 3003')
+app.listen(process.env.PORT || 3001, () => {
+    console.log('CORS-enabled web server listening on port 3001')
 })
