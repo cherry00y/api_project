aeb7e1971963ba604cb0e8868d9bcd41fc4ac2d4
cherry00y <anuthida.w64@rsu.ac.th>
2024-05-02 22:35:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,169 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/', (req, res) => {
+    res.send('Hello world!!')
+})
+
+//get
+//cakeview
+app.get('/cakeview', (req, res) => {
+    connection.query(
+        'SELECT * FROM CakeView',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//cakepopular
+app.get('/cakepopular', (req, res) => {
+    connection.query(
+        'SELECT * FROM CakePopular',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.get('/cakepopular/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query(
+        'SELECT * FROM CakePopular WHERE id = ?', [id],
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//menu
+app.get('/menucake', (req, res) => {
+    connection.query(
+        'SELECT * FROM MenuCake',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.get('/menucake/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query(
+        'SELECT * FROM MenuCake WHERE id = ?', [id],
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//event
+app.get('/event', (req, res) => {
+    connection.query(
+        'SELECT * FROM Event',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.get('/event/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query(
+        'SELECT * FROM Event WHERE id = ?', [id],
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//register user
+app.get('/register', (req, res) => {
+    connection.query(
+        'SELECT * FROM Register',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.get('/register/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query(
+        'SELECT * FROM Register WHERE id = ?', [id],
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//favourite cake
+app.get('/favourite', (req, res) => {
+    connection.query(
+        'SELECT * FROM Favourite',
+        function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//create
+//regis
+app.post('/register', (req, res) => {
+    connection.query(
+        'INSERT INTO `Register` (`fname`, `lname`, `username`,`email`,`phonenumber`, `password`, `avatar`) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [req.body.fname, req.body.lname, req.body.username, req.body.email, req.body.phonenumber, req.body.password, req.body.avatar],
+         function (err, results, fields) {
+            if (err) {
+                console.error('Error in POST /register:', err);
+                res.status(500).send('Error adding user');
+            } else {
+                res.status(201).send(results);
+            }
+        }
+    )
+})
+//favourite cake
+app.post('/favourite', (req, res) => {
+    connection.query(
+        'INSERT INTO `Favourite` (`name`, `detail`, `price`, `avatar`) VALUES (?, ?, ?, ?)',
+        [req.body.name, req.body.detail, req.body.price,  req.body.avatar],
+         function (err, results, fields) {
+            if (err) {
+                console.error('Error in POST /favourite:', err);
+                res.status(500).send('Error adding user');
+            } else {
+                res.status(201).send(results);
+            }
+        }
+    )
+})
+
+//update
+app.put('/register', (req, res) => {
+    connection.query(
+        'UPDATE `Register` SET `fname`=?, `lname`=?, `username`=?, `email`=?,`phonenumber`=?,`password`=?, `avatar`=? WHERE id =?',
+        [req.body.fname, req.body.lname, req.body.username, req.body.email, req.body.phonenumber, req.body.password, req.body.avatar, req.body.id],
+         function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+//delete
+app.delete('/favourite', (req, res) => {
+    connection.query(
+        'DELETE FROM `Favourite` WHERE id =?',
+        [req.body.id],
+         function (err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log('CORS-enabled web server listening on port 3003')
+})
